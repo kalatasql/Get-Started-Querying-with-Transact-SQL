7e17b57636258cc38b7a3837554faab6f8e567cd
kalatasql <kalata.sql@gmail.com>
2024-12-20 06:24:09 +0200

    Update Exercise- Use Subqueries.sql
    Fixed indent

--- 04-Write Subqueries in T-SQL/Exercise- Use Subqueries.sql
+++ 04-Write Subqueries in T-SQL/Exercise- Use Subqueries.sql
@@ -20,7 +20,7 @@
 SELECT ProductID, [Name], ListPrice
 FROM SalesLT.[Product]
 WHERE ListPrice > (SELECT AVG(UnitPrice) 
-				   FROM SalesLT.SalesOrderDetail)
+		   FROM SalesLT.SalesOrderDetail)
 ORDER BY ProductID;
 
 --2. Retrieve Products with a list price of 100 or more that have been sold for less than 100.
@@ -33,7 +33,7 @@ FROM SalesLT.[Product] AS p
 WHERE p.ListPrice >= 100 
   AND EXISTS 
 	  (SELECT * -- can be 1 too
-	   FROM SalesLT.SalesOrderDetail so
+	   FROM  SalesLT.SalesOrderDetail so
 	   WHERE p.ProductID = so.ProductID
 	     AND so.UnitPrice < 100);
 
@@ -41,16 +41,16 @@ SELECT p.ProductID, p.[Name], p.ListPrice
 FROM SalesLT.[Product] AS p
 WHERE p.ListPrice >= 100 
   AND ProductID IN (SELECT ProductID
-					FROM  SalesLT.SalesOrderDetail so
-					WHERE so.UnitPrice < 100);
+		    FROM  SalesLT.SalesOrderDetail so
+		    WHERE so.UnitPrice < 100);
 
-	--Challenge 2: Analyze profitability
+--Challenge 2: Analyze profitability
 
-	--The standard cost of a product and the unit price at which it is sold determine its profitability.
-	--You must use correlated subqueries to compare the cost and average selling price for each product.
+--The standard cost of a product and the unit price at which it is sold determine its profitability.
+--You must use correlated subqueries to compare the cost and average selling price for each product.
 
-	--1. Retrieve the cost, list price, and average selling price for each product
-	--Retrieve the product ID, name, cost, and list price for each product along with the average unit price for which that product has been sold.
+--1. Retrieve the cost, list price, and average selling price for each product
+--Retrieve the product ID, name, cost, and list price for each product along with the average unit price for which that product has been sold.
 
 SELECT ProductID, Name, StandardCost, ListPrice,
 			  (SELECT AVG(UnitPrice)
@@ -58,8 +58,8 @@ SELECT ProductID, Name, StandardCost, ListPrice,
 			   WHERE p.ProductID = so.ProductID) AS AverageSellingPrice
 FROM   SalesLT.Product p
 WHERE  EXISTS (SELECT * 
-			   FROM SalesLT.SalesOrderDetail so
-			   WHERE p.ProductID = so.ProductID);
+	       FROM SalesLT.SalesOrderDetail so
+	       WHERE p.ProductID = so.ProductID);
 
 --2. Retrieve products that have an average selling price that is lower than the cost.
 --Filter your previous query to include only products where the cost price is higher than the average selling price.
@@ -70,5 +70,5 @@ SELECT ProductID, Name, StandardCost, ListPrice,
 			   WHERE p.ProductID = so.ProductID) AS AverageSellingPrice
 FROM   SalesLT.Product p
 WHERE  StandardCost > (SELECT AVG(UnitPrice)
-					   FROM SalesLT.SalesOrderDetail so 
-					   WHERE p.ProductID = so.ProductID);
+		       FROM SalesLT.SalesOrderDetail so 
+       		       WHERE p.ProductID = so.ProductID);
